5511ea36748feab85b8e450ab1666c5c91cd3dd6
oscar <oscar172772@gmail.com>
2025-05-08 19:50:04 +0000

    fix cobformer bug

--- graphgps/network/cobformer.py
+++ graphgps/network/cobformer.py
@@ -17,11 +17,25 @@ class CoBFormer(torch.nn.Module):
         self.gnn = GeneralMultiLayer(cfg.gt.layer_type.split('+')[0].lower()+'conv', new_layer_config(dim_in = dim_in, dim_out = dim_out, has_bias = True, has_act = False, num_layers = cfg.gnn.layers, cfg = cfg))
         self.bga = BGA(dim_in, dim_out)
         self.attn = None
+        
+    def _apply_index(self, batch):
+        x = batch.x
+        y = batch.y if 'y' in batch else None
 
+        if 'split' not in batch:
+            return x, y
+
+        mask = batch[f'{batch.split}_mask']
+        return x[mask], y[mask] if y is not None else None
+    
     def forward(self, batch):
         tmpbatch = batch.clone()
-        z1 = self.gnn(tmpbatch).x
-        z2 = self.bga(batch).x
+        batch1 = self.gnn(tmpbatch)
+        batch2 = self.bga(batch)
+        
+        z1 = batch1.x
+        z2 = batch2.x
         extra_loss = (F.cross_entropy(z1*self.tau, F.softmax(z2*self.tau, dim=1)) + F.cross_entropy(z2*self.tau, F.softmax(z1*self.tau, dim=1)))*(1-self.alpha)/self.alpha
 
-        return z2, batch.y, extra_loss
+        pred, true = self._apply_index(batch2)
+        return pred, true, extra_loss
